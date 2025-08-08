18fa9dd9074020edbb6d6c584fd437b545381da0
Prajwal H V <prajwalharihalli@gmail.com>
2025-08-08 10:01:51 +0530

    Add Spring Boot backend skeleton

---
+++ etm-backend/src/main/java/com/prajwal/etm_backend/EtmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prajwal.etm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtmBackendApplication.class, args);
+	}
+
+}
---
+++ etm-backend/src/test/java/com/prajwal/etm_backend/EtmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajwal.etm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
